4dd12b471acf9a0357bf32c13b9a971c4c9bc92d
Tracey Jaquith <tracey@archive.org>
2018-09-22 14:24:40 -0700

    initial setup

--- TVArchive/application.js
+++ TVArchive/application.js
@@ -1,78 +1,93 @@
-//# sourceURL=application.js
-
-//
-//  application.js
-//  TVArchive
-//
-//  Created by tracey on 9/22/18.
-//  Copyright © 2018 tracey. All rights reserved.
-//
-
-/*
- * This file provides an example skeletal stub for the server-side implementation 
- * of a TVML application.
- *
- * A javascript file such as this should be provided at the tvBootURL that is 
- * configured in the AppDelegate of the TVML application. Note that  the various 
- * javascript functions here are referenced by name in the AppDelegate. This skeletal 
- * implementation shows the basic entry points that you will want to handle 
- * application lifecycle events.
- */
 
 /**
- * @description The onLaunch callback is invoked after the application JavaScript 
- * has been parsed into a JavaScript context. The handler is passed an object 
- * that contains options passed in for launch. These options are defined in the
- * swift or objective-c client code. Options can be used to communicate to
- * your JavaScript code that data and as well as state information, like if the 
- * the app is being launched in the background.
- *
- * The location attribute is automatically added to the object and represents 
- * the URL that was used to retrieve the application JavaScript.
- */
-App.onLaunch = function(options) {
-    var alert = createAlert("Hello World!", "Welcome to tvOS");
-    navigationDocument.pushDocument(alert);
-}
+  lint like:
+  cd ~/dev/TVArchive  &&  /petabox/www/node_modules/.bin/eslint .
+*/
 
+/* global App navigationDocument */
 
-App.onWillResignActive = function() {
-
-}
-
-App.onDidEnterBackground = function() {
-
-}
-
-App.onWillEnterForeground = function() {
-    
-}
-
-App.onDidBecomeActive = function() {
-    
-}
-
-App.onWillTerminate = function() {
-    
-}
-
+let baseURL
 
 /**
- * This convenience funnction returns an alert template, which can be used to present errors to the user.
+ * convenience function inserts alert template, to present messages/errors to the user.
  */
-var createAlert = function(title, description) {
+const createAlert = function (title, description) {
+  const alertString = `
+<?xml version="1.0" encoding="UTF-8"?>
+<document>
+  <alertTemplate>
+    <title>${title}</title>
+    <description>${description}</description>
+  </alertTemplate>
+</document>
+`
+  const parser = new DOMParser()
+  const alertDoc = parser.parseFromString(alertString, 'application/xml')
+  navigationDocument.pushDocument(alertDoc)
+}
 
-    var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
-        <document>
-          <alertTemplate>
-            <title>${title}</title>
-            <description>${description}</description>
-          </alertTemplate>
-        </document>`
 
-    var parser = new DOMParser();
+function parseJSON(information) {
+  const result = JSON.parse(information)
 
-    var alertDoc = parser.parseFromString(alertString, "application/xml");
+  let vids = ''
+  // eslint-disable-next-line  guard-for-in
+  for (let network in result) {
+    createAlert('parsing', network)
+    const shows = result[network]
 
-    return alertDoc
+    // <title>${network}</title>
+    vids += `
+<shelf>
+  <section>
+`
+    // eslint-disable-next-line  guard-for-in
+    for (let show in shows) {
+      vids += `
+<lockup>
+  <a href="https://archive.org/details/${show.identifier}">
+    <img src="https://archive.org/services/img/${show.identifier}" width="360" height="248"/>
+    <title>${show.title}</title>
+  </a>
+</lockup>`
+    }
+
+    vids += `
+  </section>
+</shelf>
+`
+  }
+
+  const template = `
+<document>
+  <stackTemplate>
+    <banner>
+      <title>TV News Archive - Last Week</title>
+    </banner>
+    <collectionList>
+      ${vids}
+    </collectionList>
+  </stackTemplate>
+</document>`
+  const templateParser = new DOMParser()
+  const parsedTemplate = templateParser.parseFromString(template, 'application/xml')
+  navigationDocument.pushDocument(parsedTemplate)
+}
+
+
+function getDocument(url) {
+  const templateXHR = new XMLHttpRequest()
+  templateXHR.responseType = 'document'
+  templateXHR.addEventListener('load', function() { parseJSON(templateXHR.responseText) }, false)
+  templateXHR.open('GET', url, true)
+  templateXHR.send()
+}
+
+
+App.onLaunch = function (options) {
+  createAlert('hmm', 'hai')
+  baseURL = options.BASEURL
+  const jsonURL = `${baseURL}lastweek.json`
+  createAlert('loading', 'news from last week')
+  // getDocument(jsonURL)
 }
